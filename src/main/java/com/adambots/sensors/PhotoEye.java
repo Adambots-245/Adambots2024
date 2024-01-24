c7e56b25cde7bccc43cf6973ec9a10c3d9234bbd
Tommy R <drumming.dog101@gmail.com>
2024-01-23 19:02:11 -0500

    Added an inverted option to the photoeye class

--- src/main/java/com/adambots/sensors/PhotoEye.java
+++ src/main/java/com/adambots/sensors/PhotoEye.java
@@ -16,20 +16,29 @@ public class PhotoEye {
     private DigitalInput photoEye;
     private Boolean inverted;
 
+    /**
+     * Define a PhotoEye sensor on a given DigitalInput port. 
+     * If you set up the PhotoEye to return true when not detecting as to ensure a non-critical failure mode, 
+     * you can define it as inverted to maintain the expected (returns true when detecting) return in code.
+     * <p>
+     * (photoeyes return true when disconnected)
+     * @param port
+     * The digital input port of the PhotoEye
+     * @param inverted
+     * Whether or not to invert the PhotoEye return
+     */
     public PhotoEye (int port, boolean inverted){
         this.photoEye = new DigitalInput(port);
         this.inverted = inverted;
     }
 
+    /**
+     * Returns the detection of the photoeye with regards to whether or not the PhotoEye is defined as inverted
+     *
+     * @return Whether or not the PhotoEye is detecting something
+     */
     public boolean isDetecting(){
-        if (inverted) {
-            return photoEye.get(); 
-        }
+        if (inverted) return !photoEye.get();
         return photoEye.get(); 
-    }
-
-    public DigitalInput getDigitalInput()
-    {
-        return photoEye;
     }
 }
