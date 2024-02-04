8fdb211dda01676fd798dc3dfa76c9b7d20a2a7b
Tommy R <drumming.dog101@gmail.com>
2024-02-03 22:26:48 -0500

    Tidied VisionHelpers

--- src/main/java/com/adambots/utils/VisionHelpers.java
+++ src/main/java/com/adambots/utils/VisionHelpers.java
@@ -12,11 +12,7 @@ public class VisionHelpers {
     public static String getClassName() {
         LimelightTarget_Detector[] targetDetector = LimelightHelpers.getLatestResults("limelight").targetingResults.targets_Detector;
 
-        if (targetDetector == null) {
-            return "";
-        }
-
-        if (targetDetector.length == 0) {
+        if (targetDetector == null || targetDetector.length == 0) {
             return "";
         }
 
@@ -24,7 +20,6 @@ public class VisionHelpers {
     }
 
     public static boolean isDetectingPieces() {
-
         if (getClassName() == "") {
             return false;
         } else {
@@ -33,8 +28,7 @@ public class VisionHelpers {
     }
 
     public static boolean isDetectingPieces(String type){
-        LimelightTarget_Detector[] targetDetector = LimelightHelpers
-                .getLatestResults("limelight").targetingResults.targets_Detector;
+        LimelightTarget_Detector[] targetDetector = LimelightHelpers.getLatestResults("limelight").targetingResults.targets_Detector;
         
         int index = -1;
         if(targetDetector != null){
@@ -49,14 +43,9 @@ public class VisionHelpers {
     }
 
     public static double getXLocation() {
-        LimelightTarget_Detector[] targetDetector = LimelightHelpers
-                .getLatestResults("limelight").targetingResults.targets_Detector;
+        LimelightTarget_Detector[] targetDetector = LimelightHelpers.getLatestResults("limelight").targetingResults.targets_Detector;
 
-        if (targetDetector == null) {
-            return -1.0;
-        }
-
-        if (targetDetector.length == 0) {
+        if (targetDetector == null || targetDetector.length == 0) {
             return -1.0;
         }
 
@@ -64,8 +53,7 @@ public class VisionHelpers {
     }
 
     public static double getXLocation(String type) {
-        LimelightTarget_Detector[] targetDetector = LimelightHelpers
-                .getLatestResults("limelight").targetingResults.targets_Detector;
+        LimelightTarget_Detector[] targetDetector = LimelightHelpers.getLatestResults("limelight").targetingResults.targets_Detector;
 
         int index = -1;
         if(targetDetector != null){
@@ -84,14 +72,9 @@ public class VisionHelpers {
     }
 
     public static double getYLocation() {
-        LimelightTarget_Detector[] targetDetector = LimelightHelpers
-                .getLatestResults("limelight").targetingResults.targets_Detector;
+        LimelightTarget_Detector[] targetDetector = LimelightHelpers.getLatestResults("limelight").targetingResults.targets_Detector;
 
-        if (targetDetector == null) {
-            return -1.0;
-        }
-
-        if (targetDetector.length == 0) {
+        if (targetDetector == null || targetDetector.length == 0) {
             return -1.0;
         }
 
@@ -99,8 +82,7 @@ public class VisionHelpers {
     }
 
     public static double getYLocation(String type){
-        LimelightTarget_Detector[] targetDetector = LimelightHelpers
-                .getLatestResults("limelight").targetingResults.targets_Detector;
+        LimelightTarget_Detector[] targetDetector = LimelightHelpers.getLatestResults("limelight").targetingResults.targets_Detector;
 
         int index = -1;
         if(targetDetector != null){
@@ -118,34 +100,30 @@ public class VisionHelpers {
         return targetDetector[index].ty;
     }
 
-    public static double getDistanceToObject() {
-        // how many degrees back is your limelight rotated from perfectly vertical?
-        double limelightMountAngleDegrees = 0.1; //25.0
+    // public static double getDistanceToObject() {
+    //     // how many degrees back is your limelight rotated from perfectly vertical?
+    //     double limelightMountAngleDegrees = 0.1; //25.0
 
-        // distance from the center of the Limelight lens to the floor
-        double limelightLensHeightInches = 4;
+    //     // distance from the center of the Limelight lens to the floor
+    //     double limelightLensHeightInches = 4;
 
-        // distance from the target to the floor
-        double goalHeightInches = 4;
+    //     // distance from the target to the floor
+    //     double goalHeightInches = 4;
 
-        double angleToGoalDegrees = limelightMountAngleDegrees + getYLocation();
-        double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+    //     double angleToGoalDegrees = limelightMountAngleDegrees + getYLocation();
 
-        // calculate distance
-        return (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
-    }
+    //     // calculate distance
+    //     return (goalHeightInches - limelightLensHeightInches) / Math.tan(Math.toRadians(angleToGoalDegrees));
+    // }
+
     public static boolean isAligned(){
-        if((getHorizAngle()>-2&&getHorizAngle()<2)&&isDetected()){
-            return true; 
-        }
-        return false;
+        return Math.abs(getHorizAngle()) < 2 && isDetected();
     }
+
     public static boolean isDistanceAligned(){
-        if((getGamePieceArea()>10&&getGamePieceArea()<15)){
-            return true; 
-        }
-        return false;
+        return getGamePieceArea() > 10 && getGamePieceArea() < 15;
     }
+
     public static void setPipeline(int pipeline) {
         LimelightHelpers.setPipelineIndex("limelight", pipeline); 
     }
