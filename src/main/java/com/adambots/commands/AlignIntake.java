3285a04ef8a5411bde541bf0f08349543ba6b652
aarush000 <aarush.gota@gmail.com>
2024-02-05 22:37:34 -0500

    Started Creating an Auto Align

--- src/main/java/com/adambots/commands/AlignIntake.java
+++ src/main/java/com/adambots/commands/AlignIntake.java
@@ -7,32 +7,33 @@ package com.adambots.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import com.adambots.subsystems.IntakeSubsystem;
 
-public class RunIntakeCommand extends Command {
+public class AlignIntake extends Command {
   private IntakeSubsystem intakeSubsystem;
-  private double groundIntakeMotorSpeed;
   /** Creates a new RunIntakeCommand. */
-  public RunIntakeCommand(IntakeSubsystem intakeSubsystem, double groundIntakeMotorSpeed) {
+  public AlignIntake(IntakeSubsystem intakeSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(intakeSubsystem);
     this.intakeSubsystem = intakeSubsystem;
-    this.groundIntakeMotorSpeed = groundIntakeMotorSpeed;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    intakeSubsystem.setGroundIntakeMotorSpeed(-0.1);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    System.out.println("asdlfhaskjhfisadhfkjdshafkjds");
+    if (intakeSubsystem.isPieceInRobot() == false){
+      intakeSubsystem.setGroundIntakeMotorSpeed(0);
+    }  
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intakeSubsystem.setGroundIntakeMotorSpeed(0);
-  
+    
   }
 
   // Returns true when the command should end.
