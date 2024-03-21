5328b3e07975da020f56e153c3cd4a15b58f0a35
Tommy Radabaugh <77948158+CrazyMeowCows@users.noreply.github.com>
2024-03-21 14:23:25 -0400

    Update ShooterSubsystem.java

--- src/main/java/com/adambots/subsystems/ShooterSubsystem.java
+++ src/main/java/com/adambots/subsystems/ShooterSubsystem.java
@@ -15,7 +15,7 @@ public class ShooterSubsystem extends SubsystemBase {
   private BaseMotor shooterMotor;
 
   private double shooterSpeed;  
-  private double targetWheelSpeed = 0;
+  private double targetWheelSpeed;
 
   private PIDController pidController = new PIDController(0.0, 0.01, 0.0);
 
@@ -28,6 +28,8 @@ public class ShooterSubsystem extends SubsystemBase {
     shooterMotor.enableVoltageCompensation(12.6);
 
     pidController.setIntegratorRange(0, 1);
+
+    targetWheelSpeed = 0;
 
     Dash.add("Shooter Velocity", () -> getShooterVelocity());
     // Dash.add("Shooter Command", () -> shooterSpeed);
@@ -54,7 +56,7 @@ public class ShooterSubsystem extends SubsystemBase {
     if (targetWheelSpeed > 0) {
       shooterSpeed = pidController.calculate(getShooterVelocity(), targetWheelSpeed) + targetWheelSpeed/ShooterConstants.maxSpeed*0.5;
     } else {
-      // shooterSpeed = 0.0;
+      pidController.reset();
       shooterSpeed = -0.03;
     }
 
